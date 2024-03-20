9db3b139837180ddff15fac324e689d2e9390a74
Work <yakushevso@ya.ru>
2024-03-20 09:18:07 +0300

    Stage 3/5: Set the ticket price

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,27 +3,62 @@ package cinema;
 import java.util.*;
 
 public class Cinema {
-    public static void profitCalculation() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number of rows:");
-            int rows = sc.nextInt();
+    private static final char SEAT = 'S';
+    private static final char BUSY = 'B';
+    private static final int MAX_SEATS = 60;
+    private static final int TICKET_NORMAL_PRICE = 10;
+    private static final int TICKET_LOW_PRICE = 8;
 
-            System.out.println("Enter the number of seats in each row:");
-            int seat = sc.nextInt();
+    public static void printField(int rowField, int seatField, int rowPlace, int seatPlace) {
+        System.out.println("Cinema:");
 
-            int total;
-
-            if (rows * seat <= 60) {
-                total = rows * seat * 10;
+        for (int i = 0; i <= rowField; i++) {
+            if (i == 0) {
+                System.out.print("  ");
             } else {
-                total =  rows / 2 * seat * 10 + (rows % 2 == 0 ? rows / 2 : rows / 2 + 1) * seat * 8;
+                System.out.print(i + " ");
             }
 
-            System.out.printf("Total income:\n$%d", total);
+            for (int j = 0; j < seatField; j++) {
+                if (i == 0) {
+                    System.out.print(j + 1 + " ");
+                } else if (rowPlace == i && seatPlace == j + 1) {
+                    System.out.printf("%s ", BUSY);
+                } else {
+                    System.out.printf("%s ", SEAT);
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void calculatePrice(int rowsField, int seatField, int rowPlace) {
+        if (rowsField * seatField <= MAX_SEATS || rowsField / 2 >= rowPlace) {
+            System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
+        } else {
+            System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
         }
     }
 
     public static void main(String[] args) {
-        profitCalculation();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number of rows:");
+            int rowField = sc.nextInt();
+
+            System.out.println("Enter the number of seats in each row:");
+            int seatField = sc.nextInt();
+
+            printField(rowField, seatField, 0, 0);
+
+            System.out.println("Enter a row number:");
+            int rowPlace = sc.nextInt();
+
+            System.out.println("Enter a seat number in that row:");
+            int seatPlace = sc.nextInt();
+
+            calculatePrice(rowField, seatField, rowPlace);
+            printField(rowField, seatField, rowPlace, seatPlace);
+        }
     }
 }

aef3a9938e4b9d8bd24d098c2d4c9dd0ba41198d
Work <yakushevso@ya.ru>
2024-03-20 09:23:27 +0300

    Stage 4/5: Add menu to your application

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,6 +1,6 @@
 package cinema;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Cinema {
     private static final char SEAT = 'S';
@@ -9,23 +9,27 @@ public class Cinema {
     private static final int TICKET_NORMAL_PRICE = 10;
     private static final int TICKET_LOW_PRICE = 8;
 
-    public static void printField(int rowField, int seatField, int rowPlace, int seatPlace) {
+    public static void clearSeats(char[][] field) {
+        for (int i = 1; i < field.length; i++) {
+            for (int j = 1; j < field[i].length; j++) {
+                field[i][j] = SEAT;
+            }
+        }
+    }
+
+    public static void printSeats(char[][] field) {
         System.out.println("Cinema:");
 
-        for (int i = 0; i <= rowField; i++) {
-            if (i == 0) {
-                System.out.print("  ");
-            } else {
-                System.out.print(i + " ");
-            }
-
-            for (int j = 0; j < seatField; j++) {
-                if (i == 0) {
-                    System.out.print(j + 1 + " ");
-                } else if (rowPlace == i && seatPlace == j + 1) {
-                    System.out.printf("%s ", BUSY);
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (i == 0 && j == 0) {
+                    System.out.print("  ");
+                } else if (i == 0) {
+                    System.out.print(j + " ");
+                } else if (j == 0) {
+                    System.out.print(i + " ");
                 } else {
-                    System.out.printf("%s ", SEAT);
+                    System.out.print(field[i][j] + " ");
                 }
             }
 
@@ -33,32 +37,66 @@ public class Cinema {
         }
     }
 
-    public static void calculatePrice(int rowsField, int seatField, int rowPlace) {
-        if (rowsField * seatField <= MAX_SEATS || rowsField / 2 >= rowPlace) {
+    public static void buyTicket(char[][] seats, int row, int seat) {
+        if (seats.length * seats[0].length < MAX_SEATS ||
+                seats.length / 2 > row) {
             System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
         } else {
             System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
         }
+        seats[row][seat] = BUSY;
+    }
+
+    public static void menu() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number of rows:");
+            int row = sc.nextInt();
+
+            System.out.println("Enter the number of seats in each row:");
+            int seat = sc.nextInt();
+
+            char[][] seats = new char[row + 1][seat + 1];
+
+            clearSeats(seats);
+
+            while (true) {
+                System.out.println("""
+                        1. Show the seats
+                        2. Buy a ticket
+                        3. Cancel ticket purchase
+                        4. Clear Seats
+                        0. Exit""");
+
+                int num = sc.nextInt();
+
+                if (num == 0) {
+                    break;
+                } else if (num == 1) {
+                    printSeats(seats);
+                } else if (num == 2) {
+                    System.out.println("Enter a row number:");
+                    row = sc.nextInt();
+
+                    System.out.println("Enter a seat number in that row:");
+                    seat = sc.nextInt();
+
+                    buyTicket(seats, row, seat);
+                } else if (num == 3) {
+                    System.out.println("Enter the row number of the seat you would like to cancel:");
+                    row = sc.nextInt();
+
+                    System.out.println("Enter the seat number in that row:");
+                    seat = sc.nextInt();
+
+                    seats[row][seat] = SEAT;
+                } else if (num == 4) {
+                    clearSeats(seats);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number of rows:");
-            int rowField = sc.nextInt();
-
-            System.out.println("Enter the number of seats in each row:");
-            int seatField = sc.nextInt();
-
-            printField(rowField, seatField, 0, 0);
-
-            System.out.println("Enter a row number:");
-            int rowPlace = sc.nextInt();
-
-            System.out.println("Enter a seat number in that row:");
-            int seatPlace = sc.nextInt();
-
-            calculatePrice(rowField, seatField, rowPlace);
-            printField(rowField, seatField, rowPlace, seatPlace);
-        }
+        menu();
     }
 }

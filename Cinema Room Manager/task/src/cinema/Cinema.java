7e0d64f10923c34e5a88ebcf6249ce0d6cb568db
Work <yakushevso@ya.ru>
2024-03-20 09:26:12 +0300

    Stage 5/5: Manage stats and make your app error-prone

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,6 +1,6 @@
 package cinema;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Cinema {
     private static final char SEAT = 'S';
@@ -8,20 +8,35 @@ public class Cinema {
     private static final int MAX_SEATS = 60;
     private static final int TICKET_NORMAL_PRICE = 10;
     private static final int TICKET_LOW_PRICE = 8;
+    private static char[][] cinema;
+    private static int tickets;
+    private static int currentIncome;
+    private static int totalIncome;
+    public static int rowCinema;
+    public static int seatCinema;
 
-    public static void clearSeats(char[][] field) {
-        for (int i = 1; i < field.length; i++) {
-            for (int j = 1; j < field[i].length; j++) {
-                field[i][j] = SEAT;
-            }
-        }
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        menu();
     }
 
-    public static void printSeats(char[][] field) {
+    public static void clearCinema() {
+        for (int i = 1; i < cinema.length; i++) {
+            for (int j = 1; j < cinema[i].length; j++) {
+                cinema[i][j] = SEAT;
+            }
+        }
+        tickets = 0;
+        currentIncome = 0;
+        totalIncome = 0;
+    }
+
+    public static void printCinema() {
         System.out.println("Cinema:");
 
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[i].length; j++) {
+        for (int i = 0; i < cinema.length; i++) {
+            for (int j = 0; j < cinema[i].length; j++) {
                 if (i == 0 && j == 0) {
                     System.out.print("  ");
                 } else if (i == 0) {
@@ -29,7 +44,7 @@ public class Cinema {
                 } else if (j == 0) {
                     System.out.print(i + " ");
                 } else {
-                    System.out.print(field[i][j] + " ");
+                    System.out.print(cinema[i][j] + " ");
                 }
             }
 
@@ -37,66 +52,108 @@ public class Cinema {
         }
     }
 
-    public static void buyTicket(char[][] seats, int row, int seat) {
-        if (seats.length * seats[0].length < MAX_SEATS ||
-                seats.length / 2 > row) {
-            System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
-        } else {
-            System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
-        }
-        seats[row][seat] = BUSY;
-    }
+    public static void buyTicket() {
+        while (true) {
+            int row, seat;
+            try {
+                System.out.println("Enter a row number:");
+                row = sc.nextInt();
 
-    public static void menu() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number of rows:");
-            int row = sc.nextInt();
+                System.out.println("Enter a seat number in that row:");
+                seat = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
+            }
 
-            System.out.println("Enter the number of seats in each row:");
-            int seat = sc.nextInt();
-
-            char[][] seats = new char[row + 1][seat + 1];
-
-            clearSeats(seats);
-
-            while (true) {
-                System.out.println("""
-                        1. Show the seats
-                        2. Buy a ticket
-                        3. Cancel ticket purchase
-                        4. Clear Seats
-                        0. Exit""");
-
-                int num = sc.nextInt();
-
-                if (num == 0) {
-                    break;
-                } else if (num == 1) {
-                    printSeats(seats);
-                } else if (num == 2) {
-                    System.out.println("Enter a row number:");
-                    row = sc.nextInt();
-
-                    System.out.println("Enter a seat number in that row:");
-                    seat = sc.nextInt();
-
-                    buyTicket(seats, row, seat);
-                } else if (num == 3) {
-                    System.out.println("Enter the row number of the seat you would like to cancel:");
-                    row = sc.nextInt();
-
-                    System.out.println("Enter the seat number in that row:");
-                    seat = sc.nextInt();
-
-                    seats[row][seat] = SEAT;
-                } else if (num == 4) {
-                    clearSeats(seats);
+            if (row > cinema.length - 1 || row < 0 || seat > cinema[0].length - 1 || seat < 0) {
+                System.out.println("Wrong input!");
+            } else if (cinema[row][seat] == BUSY) {
+                System.out.println("That ticket has already been purchased!");
+            } else {
+                if (rowCinema * seatCinema < MAX_SEATS || rowCinema / 2 >= row) {
+                    currentIncome += TICKET_NORMAL_PRICE;
+                    System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
+                } else {
+                    currentIncome += TICKET_LOW_PRICE;
+                    System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
                 }
+
+                cinema[row][seat] = BUSY;
+                tickets++;
+                break;
             }
         }
     }
 
-    public static void main(String[] args) {
-        menu();
+    public static void printStat() {
+        if (rowCinema * seatCinema < MAX_SEATS) {
+            totalIncome = rowCinema * seatCinema * TICKET_NORMAL_PRICE;
+        } else {
+            totalIncome = rowCinema / 2 * seatCinema * TICKET_NORMAL_PRICE +
+                    (rowCinema % 2 == 0 ? rowCinema / 2 : rowCinema / 2 + 1) * seatCinema * TICKET_LOW_PRICE;
+        }
+
+        System.out.println("Number of purchased tickets: " + tickets);
+        System.out.printf("Percentage: %.2f%%\n", 100f / (float) (rowCinema * seatCinema) * (float) tickets);
+        System.out.println("Current income: $" + currentIncome);
+        System.out.println("Total income: $" + totalIncome);
+    }
+
+    public static void createCinema() {
+        while (true) {
+            try {
+                System.out.println("Enter the number of rows:");
+                rowCinema = sc.nextInt();
+
+                System.out.println("Enter the number of seats in each row:");
+                seatCinema = sc.nextInt();
+
+                if (rowCinema < 0 || seatCinema < 0) {
+                    System.out.println("Wrong input!");
+                    continue;
+                }
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+            break;
+        }
+
+        cinema = new char[rowCinema + 1][seatCinema + 1];
+    }
+
+    public static void menu() {
+        createCinema();
+        clearCinema();
+
+        while (true) {
+            System.out.println("""
+                    1. Show the seats
+                    2. Buy a ticket
+                    3. Statistics
+                    4. Clear Seats
+                    5. New Cinema
+                    0. Exit""");
+
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                break;
+            } else if (num == 1) {
+                printCinema();
+            } else if (num == 2) {
+                buyTicket();
+            } else if (num == 3) {
+                printStat();
+            } else if (num == 4) {
+                clearCinema();
+            } else if (num == 5) {
+                createCinema();
+                clearCinema();
+            }
+        }
     }
 }
